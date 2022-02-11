53f5033b77969f9b3a72af228e4b3617ed3273da
Tafuso <gustavovasquez2002@gmail.com>
2022-02-11 09:28:31 -0300

    feat: add register on login page

--- src/pages/index/components/Login/index.tsx
+++ src/pages/index/components/Login/index.tsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from './styles.module.scss'
 
 
 const Login: React.FC = () => { 
+  const [hasAccount, setHasAccount] = useState(false)
   return (
     <div className={Styles.container}>
       <div className={Styles.title}>
-        <h1>Entrar</h1>
-        <h2>Bem-vindo(a) ao seu nutricionista pessoal</h2>
+        <h1>{hasAccount ? "Entrar" : "Criar Conta"}</h1>
+        <h2>{hasAccount ? "Bem-vindo(a) ao seu nutricionista pessoal" : "Comece agora com seu nutricionista pessoal"}</h2>
       </div>
+      {hasAccount || <>
+      <span>Nome</span>
+      <input type="text" />
+      </>
+      }
       <span>Email</span>
       <input type="text" />
       <span>Senha</span>
       <input type="password" />
-      <button>Entrar</button>
-      <a href="#">Não tem uma conta? <b>Crie uma conta.</b></a>
+      {hasAccount || <>
+      <span>Confirme sua Senha</span>
+      <input type="password" />
+      </>
+      }
+      <button>{hasAccount ? "Entrar" : "Criar Conta"}</button>
+      <p 
+      onClick={e => hasAccount ? setHasAccount(false) : setHasAccount(true)}
+      >
+        {hasAccount ? <>Não tem uma conta? <b>Crie uma conta.</b></> : <>Já tem uma conta? <b>Faça login.</b></>}
+        </p>
 
     </div>
   )
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Styles from './styles.module.scss'
 import bgImg from '../../assets/img-index-page.png'
 import Login from "./components/Login";
-import Register from "./components/Register";
 
 const Index: React.FC = () => { 
   return (
