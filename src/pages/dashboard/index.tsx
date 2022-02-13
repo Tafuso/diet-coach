6f2f9bba7eab2ff7008a966e31e3909c1787cc1c
Tafuso <gustavovasquez2002@gmail.com>
2022-02-13 01:50:51 -0300

    feat: create dashboard page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from './styles.module.scss'
+
+const Dashboard: React.FC = () => { 
+  return (
+    <div className={Styles.container}>
+      Hello World
+    </div>
+  )
+}
+
+export default Dashboard
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from '../pages/dashboard'
 import Index from '../pages/index'
 
 
@@ -8,6 +9,7 @@ const Router: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
       </Routes>
     </BrowserRouter>
   )
